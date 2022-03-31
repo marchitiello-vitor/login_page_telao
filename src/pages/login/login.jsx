5da14de07c7ce937509fcb011f7194e94183f524
Vitor Marchitiello Labadessa <43055330811@info.scs>
2022-03-31 11:15:37 -0300

    1.0

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import '../../assets/css/login.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import icone from '../../assets/img/undraw.svg';
+
+function Fazer(params) {
+
+}
+
+export default function Login() {
+    return (
+  
+            <body>
+                
+            </body>
+
+    );
+}
+
