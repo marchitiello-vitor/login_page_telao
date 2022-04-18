0f73c1e096a7c3e4cd135ac93e8b05b824b9968e
Vitor Marchitiello Labadessa <43055330811@info.scs>
2022-04-18 11:08:32 -0300

    postman

--- src/index.js
+++ src/index.js
@@ -2,37 +2,41 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Login from '../src/pages/login/login.jsx'
+import Login from './pages/login/login.jsx'
 import reportWebVitals from './reportWebVitals';
 import {
   Route,
-  BrowserRouter as Router,
-  Redirect,
-  Switch,
+  BrowserRouter,
+  Routes,
 } from 'react-router-dom';
 import { parseJwt, usuarioAutenticado } from './services/auth'
 
-import './index.css'
-
-import login from './pages/login/login'
-
-const PermimssaoComum = ({component: Component}) => (
-  <Route
-    render={(props) =>
-      usuarioAutenticado() && parseJwt().role === '2' ? (
-        <Component {...props} />
-      ) : (
-        <redirect to="" />
-      )
-    }
-  />
-)
+// const PermimssaoComum = ({component: Component}) => (
+//   <Route
+//     render={(props) =>
+//       usuarioAutenticado() && parseJwt().role === '2' ? (
+//         <Component {...props} />
+//       ) : (
+//         <redirect to="" />
+//       )
+//     }
+//   />
+// )
  
-ReactDOM.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+
+  <BrowserRouter>
+    <div>
+      <Routes>
+        <Route index element={<Login/>} />
+        {/* <Route exact path="/cadastro" component={Cadastro} /> */}
+        {/* <Route path="/Login" element={<Login />}></Route> */}
+      </Routes>
+    </div>
+  </BrowserRouter>
+)
+
+ReactDOM.render( routing, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,25 +1,73 @@
-import { render } from '@testing-library/react';
+
 import '../../assets/css/login.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import icone from '../../assets/img/undraw.svg';
 import logo from '../../assets/img/Logo.svg';
+import { Component } from 'react';
+import axios from "axios";
 
 
-export default function Login() {
-    return (
+class Login extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            email:'',
+            senha:'',
+            erroMensagem: '',
+            isLoading: false
+        };
+    };
+
+    efetuarLogin = (event) => {
+        event.preventDefault();
+
+        this.setState({erroMensagem:'', isLoading: true})
+
+        axios.post('http://localhost:5000/api/Login', {
+            email: this.state.email,
+            senha: this.state.senha,
+        })
+
+        .then(resposta => {
+
+            if(resposta.status === 200){
+                localStorage.setItem('usuario-login', resposta.data.token)
+                this.setState({ isLoading: false });
+
+                let base64 = localStorage.getItem('usuario-login').split('.')[1];
+                console.log(base64);
+
+                console.log(this.props);
+            }
+        })
+
+        .catch(() => {
+            this.setState({erroMensagem: 'E-mail e/ou senha invalidos!', isLoading: false })
+        })
+
+    };
+
+    atualizaStateCampo = (campo) => {
+        this.setState({[campo.target.name]: campo.target.value})
+    }
+
+    render() {
+
+        return (
 
             <body>
                 <div class="box_vermelho">
                     <div id="box">
                         <div id="topbox">
-                            <img src={ icone } alt="imagem" class="ilus"/>
+                            <img src={icone} alt="imagem" class="ilus" />
                         </div>
                     </div>
 
                     <div id="lado_vermelho">
                         <div class="logo_text">
-                            <img class="logo" src={ logo } alt="logo" />
+                            <img class="logo" src={logo} alt="logo" />
                             <p class="text_respon">FAÇA O SEU LOGIN PARA ACESSAR A REDE</p>
                         </div>
                         <label>
@@ -32,11 +80,18 @@ export default function Login() {
                             <input type="password" placeholder="" class="puts" />
                         </label>
 
-                        <button type="submit" value="Conectar" class="botao" id="botao_1">Entrar</button>
+                        <button
+                            type="submit"
+                            value="Conectar"
+                            class="botao"
+                            id="botao_1">Entrar</button>
                     </div>
                 </div>
             </body>
 
-    );
+        );
+    }
 }
+
+export default Login;
 
